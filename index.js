b69ec315204d7176eecfcab5bb8fc4deeea1029a
ruixenui <govindalapudisrinath@gmail.com>
2025-06-23 21:24:03 +0530

    bug fi23-06/21:33

--- index.js
+++
@@ -1,8 +0,0 @@
-// Basic Cloudflare Worker
-export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello from Ruixen!', {
-      headers: { 'content-type': 'text/plain' },
-    });
-  },
-};
--- worker.ts
+++
@@ -1,8 +0,0 @@
-// Basic Cloudflare Worker
-export default {
-  async fetch(request: Request, env: any, ctx: any) {
-    return new Response('Hello from Ruixen!', {
-      headers: { 'content-type': 'text/plain' },
-    });
-  },
-};
